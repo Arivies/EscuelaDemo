3f45c213e15f432f0b9a0f66c3ab067bd5b100fa
Jorge Atempa <jatempa@itmexicali.edu.mx>
2013-12-20 13:17:16 -0800

    Habilitando paginador

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new ITM\AlumnosBundle\AlumnosBundle(),
             new ITM\CarrerasBundle\CarrerasBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
