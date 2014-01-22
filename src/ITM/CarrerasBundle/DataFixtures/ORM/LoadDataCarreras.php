bf2ee160b081f44bfd5afcd0806c1389ceb399b4
Jorge Atempa <jatempa@itmexicali.edu.mx>
2014-01-22 14:48:27 -0800

    Ajustando campos a las tablas y modificadon datos de las fixtures

--- src/ITM/CarrerasBundle/DataFixtures/ORM/LoadDataCarreras.php
+++ src/ITM/CarrerasBundle/DataFixtures/ORM/LoadDataCarreras.php
@@ -12,17 +12,17 @@ class LoadDataCarreras extends AbstractFixture implements OrderedFixtureInterfac
     public function load(ObjectManager $manager)
     {
         $carreras = array(
-            array('nombre' => 'Ingeniería Industrial'),
-            array('nombre' => 'Ingeniería en Logística'),
-            array('nombre' => 'Ingeniería Mecatrónica'),
-            array('nombre' => 'Ingeniería Mecánica'),
-            array('nombre' => 'Ingeniería Sistemas Computacionales'),
-            array('nombre' => 'Ingeniería en Gestión Empresarial'),
-            array('nombre' => 'Ingenieria en Informática'),
-            array('nombre' => 'Ingenieria en Energías Renovables'),
-            array('nombre' => 'Ingeniería Electrónica'),
-            array('nombre' => 'Ingeniería Química'),
-            array('nombre' => 'Ingeniería Eléctrica')
+            array('nombre' => 'Ing. Industrial'),
+            array('nombre' => 'Ing. en Logística'),
+            array('nombre' => 'Ing. Mecatrónica'),
+            array('nombre' => 'Ing. Mecánica'),
+            array('nombre' => 'Ing. en Sistemas Computacionales'),
+            array('nombre' => 'Ing. en Gestión Empresarial'),
+            array('nombre' => 'Ing. en Informática'),
+            array('nombre' => 'Ing. en Energías Renovables'),
+            array('nombre' => 'Ing. Electrónica'),
+            array('nombre' => 'Ing. Química'),
+            array('nombre' => 'Ing. Eléctrica')
         );
 
         foreach ($carreras as $carrera) {
